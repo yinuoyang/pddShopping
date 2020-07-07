f35be6c0c59b1e27ceb51f4c41595738bca25ed7
yinuoyang <yyn4a55@hotmail.com>
2020-07-07 00:51:33 -0400

    update the header tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+  title: string;
+  link?: string;
+}
+
+interface AddFunc {
+  (x: number, y: number): number;
+}
+
+interface Dict {
+  [key: string]: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pddShopping';
+  menus: TopMenu[] = [
+    { title: 'Featured', link: '' },
+    { title: "Men's suit", link: '' },
+    { title: 'Mobile Phone', link: '' },
+    { title: 'Women suit', link: '' },
+    { title: 'Toys', link: '' },
+    { title: 'Games', link: '' },
+    { title: 'Bang!', link: '' },
+  ];
+
+  add: AddFunc = (x, y) => x + y;
+  selectIndex = -1;
 }
